ba749bcd6f5d93a186e09a499b7ee07fe2f16f7b
ccottle <ramm@gmail.com>
2025-11-04 08:56:51 -0700

    implemented abs player class

--- Player.py
+++ Player.py
@@ -1,1 +1,14 @@
-#Parent class for each agent
+from abc import ABC, abstractmethod
+from typing import Any
+
+class Player(ABC):
+	def __init__(self, name: str = "") -> None:
+		self.name = name
+
+	@abstractmethod
+	def defect(self) -> Any:
+		pass
+
+	@abstractmethod
+	def cooperate(self) -> Any:
+		pass
